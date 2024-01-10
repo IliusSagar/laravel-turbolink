57f1d20fc91338fd44737fb6327899bb38e9eaad
IliusSagar <sagar01830@gmail.com>
2024-01-10 13:18:17 +0600

    Turbolink js include & css or script link include

---
+++ resources/views/Notepad.blade.php
@@ -0,0 +1,11 @@
+** composer create-project laravel/laravel example-app
+** npm install --save turbolinks
+** npm install
+** npm run dev
+** npm run dev
+** npm run watch (optional)
+** resources/js/app.js
+	var Turbolinks = require("turbolinks");
+	Turbolinks.start();
+** npm run dev
+** npm run dev
